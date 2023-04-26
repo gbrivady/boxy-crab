527b452b25d3793f03db3e1f04c3b14c94d57738
gbrivady <12777915+gbrivady@users.noreply.github.com>
2023-04-26 17:44:00 +0000

    Add vertical hints creation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ type Grid = Vec<Vec<bool>>;
 
 fn build_hints(g: Grid) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
     let mut row_hints: Vec<Vec<i32>> = Vec::new();
-    // let mut col_hints: Vec<Vec<i32>> = vec![Vec::new(); g[0].len()];
+    let mut col_hints: Vec<Vec<i32>> = vec![Vec::new(); g[0].len()];
     let mut col_counts: Vec<i32> = vec![0; g[0].len()];
     for row in g.iter() {
         let mut cur_hint: Vec<i32> = Vec::new();
@@ -13,10 +13,19 @@ fn build_hints(g: Grid) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
                     cur_count += 1;
                     col_counts[j] += 1;
                 }
-                ((j, &false), 0) => (),
+                ((j, &false), 0) => {
+                    if col_counts[j] != 0 {
+                        col_hints[j].push(col_counts[j]);
+                        col_counts[j] = 0;
+                    }
+                }
                 ((j, &false), _) => {
                     cur_hint.push(cur_count);
                     cur_count = 0;
+                    if col_counts[j] != 0 {
+                        col_hints[j].push(col_counts[j]);
+                        col_counts[j] = 0;
+                    }
                 }
             }
         }
@@ -25,16 +34,17 @@ fn build_hints(g: Grid) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
         }
         row_hints.push(cur_hint);
     }
-    return (row_hints, vec![vec![0]; 1]);
+    for (j, count) in col_counts.iter().enumerate() {
+        if *count != 0 {
+            col_hints[j].push(*count);
+        }
+    }
+    return (row_hints, col_hints);
 }
 
 fn main() {
-    println!(
-        "{:#?}",
-        build_hints(vec![
-            vec![true, true, false, true, true, false],
-            vec![true, true, false, false, false, true]
-        ])
-        .0
-    )
+    let (a, b) = build_hints(vec![
+        vec![true, true, false, true, true, false],
+        vec![true, true, false, false, false, true]]);
+    println!("{:#?}\n{:#?}", a, b);
 }
