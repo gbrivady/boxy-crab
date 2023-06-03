401640187c3f3d4023e2a113abee1060a6f5e0a4
g.brivady <12777915+gbrivady@users.noreply.github.com>
2023-06-03 04:08:25 +0200

    Clean, comment and reorganize main file

--- src/main.rs
+++ src/main.rs
@@ -29,36 +29,41 @@ fn legal_move(input: Move, cursor: (usize, usize), grid_dim: (usize, usize)) -> 
 }
 
 fn main() {
-    print!("\u{001b}[?1049h"); //Go to secondary screen, hide cursor
+    print!("\u{001b}[?1049h"); //Go to secondary screen
 
-    // print!("\u{001b}[?25h"); //Set cursor back to visible for debugging purposes
+    print!("\u{001b}[2J"); //Clear screen
 
+    // Build hints and two grid : puzzle grid and user grid
     let grid: Grid = const_grid_to_vec!(Grid::DBG);
     let (h_hints, v_hints) = grid.build_hints();
-    print!("\u{001b}[2J");
-    let (x, y) = tdisplay::draw_hints(h_hints, v_hints);
-    print!("\u{001b}[{};{}f", x + 2, y + 2);
-
-    io::stdout().flush().expect("Unable to flush stdout");
-    enable_raw_mode().expect("Unable to enable raw mode");
-
-    let mut buffer: [u8; 1] = [0u8; 1];
-    let mut handle: std::io::Stdin = io::stdin();
-    let mut input: Move;
-    // MAIN GAME LOOP
-
-    let mut cursor: (usize, usize) = (0, 0);
-
     let grid_dim: (usize, usize) = (
         grid.len().try_into().unwrap(),
         grid[0].len().try_into().unwrap(),
     );
     let mut user_grid: Grid = Grid::make_empty(grid_dim.0, grid_dim.1);
+
+    // Display hints and set cursor to the right position
+    let (x, y) = tdisplay::draw_hints(h_hints, v_hints);
+    print!("\u{001b}[{};{}f", x + 2, y + 2);
     print!("{user_grid}");
     print!("\u{001b}[{}A", grid_dim.0);
+    io::stdout().flush().expect("Unable to flush stdout");
+
+    // Setup game variables
+    // 1 - IO
+    let mut buffer: [u8; 1] = [0u8; 1];
+    let mut handle: std::io::Stdin = io::stdin();
+    let mut input: Move;
+
+    // 2 - Game
+    let mut cursor: (usize, usize) = (0, 0);
     let mut old_cell: grid::Cell;
     let mut new_cell: grid::Cell = user_grid[cursor.0][cursor.1];
-    io::stdout().flush().expect("Unable to flush stdout");
+
+    // Set raw mode before main loop
+    enable_raw_mode().expect("Unable to enable raw mode");
+
+    // Game main loop
     loop {
         old_cell = new_cell;
         handle
@@ -123,14 +128,12 @@ fn main() {
         tdisplay::update_cursor(input, old_cell, new_cell);
         io::stdout().flush().expect("Unable to flush stdout");
     }
-    //Move cursor:
-    // Redraw current cell in normal color
-    // Move cursor
-    // Draw over new cell
 
+    // Exit raw mode, flush any leftover input
     disable_raw_mode().expect("Unable to disable raw mode");
     io::stdout().flush().expect("Unable to flush stdout");
 
+    // Slight pause after to display quitting text
     thread::sleep(time::Duration::from_secs(1));
-    print!("\u{001b}[?25h\u{001b}[?1049l"); //Back to main screen, de-hides cursor
+    println!("\u{001b}[?25h\u{001b}[?1049l"); //Back to main screen, de-hides cursor
 }
